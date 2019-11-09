dd8ef9573d3f3b2d0a8cca679cda9cefee37c29c
Mild Wilbur <mwilbur@ox-c6.21-school.ru>
2019-11-09 17:09:47 +0300

    complete

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwilbur <mwilbur@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/20 17:21:16 by mwilbur           #+#    #+#             */
+/*   Updated: 2019/11/01 17:48:26 by mwilbur          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 21
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+int		get_str(char **str, char **line, int ret);
+int		get_next_line(const int fd, char **line);
+
+#endif
